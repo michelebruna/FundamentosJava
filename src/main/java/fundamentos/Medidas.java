f8de2691b6f7c7e488acefe57ca5ee7fc1037dfd
Michele <michelebrunasilva@gmail.com>
2022-03-07 16:15:26 -0300

    Initial commit

---
+++ src/main/java/fundamentos/Medidas.java
@@ -0,0 +1,19 @@
+//1 - Pacote
+package fundamentos;
+
+//2 - Bibliotecas
+
+//3 - Classe
+public class Medidas {
+    //3.1 - Atributos
+
+    //3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Escolha o cálculo desejado:");
+        System.out.println("1 - Área do Quadrado");
+        System.out.println("2 - Área do Retângulo");
+        System.out.println("3 - Área do Triângulo");
+        System.out.println("4 - Área do Círculo");
+
+    }
+}
